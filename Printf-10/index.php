a4d83f0ac65354839c485fa7e774c5ce9ec36fe1
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-18 16:10:59 +0600

    Printf-10

---
+++ Printf-10/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "chadni";
+$school_name = "Testl";
+//printf ("Helllo $name");
+//$new_name = ucwords($name);  //ucwords()  capital 1st letter
+//echo "Hello $new_name";
+//echo "Hello ". ucwords( $name);
+
+printf ("Hello %s", $name);
+printf ("Hello %s My school Name: %s My address is %s",ucwords($name), $school_name, "sunamganj" );
+
+?>
--- Var-Dump-09/index.php
+++ Var-Dump-09/index.php
@@ -2,7 +2,9 @@
 $name = "Puja";
 $num = 20;
 
-var_dump($name);
-echo "<br>";
-var_dump($num);
+// var_dump($name);
+// echo "<br>";
+// var_dump($num);
+
+var_dump($name, $num)
 ?>
