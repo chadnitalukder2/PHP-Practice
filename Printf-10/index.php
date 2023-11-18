f1c935f90af6d6e764d375105399394c36d69f4f
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-18 18:34:09 +0600

    Printf-10

--- Printf-10/index.php
+++ Printf-10/index.php
@@ -1,17 +1,17 @@
 <?php
 
-// $name = "chadni";
-// $school_name = "Testl";
-//printf ("Helllo $name");
-//$new_name = ucwords($name);  //ucwords()  capital 1st letter
-//echo "Hello $new_name";
-//echo "Hello ". ucwords( $name);
+/*$name = "chadni";
+$school_name = "Testl";
+printf ("Helllo $name");
+$new_name = ucwords($name);  //ucwords()  capital 1st letter
+echo "Hello $new_name";
+echo "Hello ". ucwords( $name);
 
-// printf ("Hello %s", $name);
-// printf ("Hello %s My school Name: %s My address is %s",ucwords($name), $school_name, "sunamganj" );
-
-$first_name = "nitesh";
-$last_name = "talukder";
+printf ("Hello %s", $name);
+printf ("Hello %s My school Name: %s My address is %s",ucwords($name), $school_name, "sunamganj" );
+*/
+//$first_name = "nitesh";
+//$last_name = "talukder";
 
 /*$html_block = "<div>
 <h1>".ucwords ($first_name) ."</h1>
@@ -19,10 +19,27 @@ $last_name = "talukder";
 </div>";
 echo $html_block;*/
 
-$html_block = "<div>
+/*$html_block = "<div>
 <h1> %s</h1>
 <h2> %s</h2>
 </div>";
 printf($html_block, ucwords($first_name ), ucwords($last_name))
+*/
+/*========================================================*/
+//intejar = d
+//flort = f
+//string = s
+//bolian = b
+//asci valu = c  means a=97, b= 98, A=65
+//==========================================================
+/*$amount = 65;
+printf ("Hello %c" ,$amount );*/
+
+
+
+
 
 ?>
+
+
+
