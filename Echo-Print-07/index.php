18ad276560377aba96120fa3193da48152d7d88c
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-18 14:43:02 +0600

    Var-Dump-09

---
+++ Echo-Print-07/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$test = print("Hello");
+echo "<br>";
+echo $test;
+echo "<br>";
+echo "<h1 >Puja</h1>";
+echo "<h1 class=\"text-color\" >Puja</h1>";
+echo "<br>";
+
+$name ="Nitesh";
+echo "<h1>".$name."</h1>"
+?>
---
+++ Var-Dump-09/index.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Puja";
+$num = 20;
+
+var_dump($name);
+echo "<br>";
+var_dump($num);
+?>
