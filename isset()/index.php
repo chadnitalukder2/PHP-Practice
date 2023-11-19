c1a3e017962b265c3d3f3e61265aeb99bc6ab617
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-20 02:25:28 +0600

    isset

---
+++ isset()/index.php
@@ -0,0 +1,48 @@
+<?php
+/*$val1 = null;
+isset ($val1); //value takle frue na takle false
+
+$result = $val1 + 5;
+echo $result;*/
+?> 
+<!-- ======================================================== -->
+<?php
+//$val1 = null;
+/*$val1 = 10;
+
+var_dump(isset($val2));
+
+if(isset($val1)){
+    $result = $val1 + 5;
+    echo $result;
+}*/
+
+?>
+
+
+<!-- ====================================================== -->
+<?php
+//$my_name = "Nitesh";
+
+if(!isset($my_name)){
+    $my_name = "Puja";
+}
+echo $my_name;
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
