9e055f0e2a549534ff50df97a1115a878b99fef4
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-19 23:53:17 +0600

    switch

--- Switch/index.php
+++ Switch/index.php
@@ -1,5 +1,5 @@
 <?php
-$name = "Nitesh";
+/*$name = "Nitesh";
 
 switch ($name) {
     case "Puja";
@@ -11,8 +11,55 @@ case "Nitesh";
     break;
 default:
     echo "Bey";
+}*/
+?>
+<!-- ==================================== -->
+
+<?php
+
+/*$num = 3;
+
+switch($num){
+    case 1:
+        echo "One";
+        break;
+    case 2:
+        echo "Two";
+        break;
+    case 3 :
+        echo "Three";
+        break;
+    case 4: 
+        echo "Four";
+        break;
+    default:
+
+}*/
+?>
+
+<!-- ===================================== -->
+<?php
+
+$num = "3PHP";
+
+switch($num){
+    case "1":
+        echo "One";
+        break;
+    case "2":
+        echo "Two";
+        break;
+    case "3" :
+        echo "Three";
+        break;
+    case "4": 
+        echo "Four";
+        break;
+    default:
+    echo "hello";
 }
 
 
-
 ?>
+
+
