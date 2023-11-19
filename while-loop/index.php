ab9d037c15b365134c10c83e76c05def2cca1f99
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-19 17:15:48 +0600

    while

--- while-loop/index.php
+++ while-loop/index.php
@@ -1,8 +1,33 @@
 <?php
-$count = 1;
+/*$count = 1;
 while($count <= 5){
 echo "Puja $count <br>";
 $count++;
-}
-
+}*/
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nested if</title>
+</head>
+<body>
+    <from action="#">
+    year
+<select>
+    <?php
+    $year = 2000;
+    while(  $year <= 2050 ){
+    ?>
+    <option value="<?php echo  $year ;?>"><?php echo  $year ;?></option>
+    <?php
+    $year++;
+    }
+    ?>
+
+</select>
+
+</from>
+</body>
+</html>
