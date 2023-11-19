4919c2b232fa9971bb4b4a2f7fc037f8c881d805
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-19 11:26:59 +0600

    nested

---
+++ Nested-if/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$username = "abcd";
+$password = "12345";
+
+/*if($username == 'abcde' && $password == 12345){
+echo "hello user";
+}
+else{
+    echo "Invalid username / password";
+}*/
+
+// Nested
+
+if($username == 'abcd'){
+    if($password == 12045){
+        echo "Hello user";
+    }
+    else{
+        echo "Invalid Password";
+    }
+}
+else{
+    echo "Invalid Username";
+}
+
+?>
