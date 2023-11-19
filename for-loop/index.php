d18b118bdb39e74ac272d3f28cca5d8c377095d8
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-19 17:56:56 +0600

    for

---
+++ for-loop/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Loop</title>
+</head>
+<body>
+    <ul>
+            <?php
+                for($user_count = 1; $user_count <= 50; $user_count++ ){
+            ?>
+                    <li> user <?php echo $user_count ?></li>
+            <?php
+                }
+            ?>
+
+
+</ul>
+</body>
+</html>
