d6b6b41164d1de68a9871b95233cf2e99bbd23ef
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-20 01:59:57 +0600

    alternative-syntax-switch

---
+++ alternative-syntax/index.php
@@ -0,0 +1,83 @@
+<!-- ========================== If else============================ -->
+<?php   /* if (false): ?>
+    <h1>Hello-1 </h1>
+<?php elseif (true): ?>
+    <h1>Hello-2 </h1>
+<?php elseif (true): ?>
+    <h1>Hello-3 </h1>
+<?php else: ?>
+    echo "Bye";
+<?php endif; */  ?>
+
+
+<!-- ============================================ -->
+<?php
+/*if (false):
+    echo "Hello";
+elseif (true):
+    echo "Hello-1";
+elseif (true):
+        echo "Hello-2";
+else:
+    echo "Bye";
+endif;*/
+?>
+
+<!-- ==================switch==================== -->
+<?php
+switch(2):
+    case 1:
+        echo "hello-1";
+        break;
+    case 2:
+        echo "hello-2";
+        break;
+    default:
+        echo "Bye";
+ endswitch;
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
