0e7ce230caeeed6916d346ebde9a73e7c4de4696
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-19 18:13:21 +0600

    continue

--- break/index.php
+++ break/index.php
@@ -1,9 +1,23 @@
 <?php
-for($count = 1 ; $count <= 10 ; $count++){
+/*for($count = 1 ; $count <= 10 ; $count++){
     echo "Hello".$count."<br>";
     if($count == 5){
         break;
     }
+}*/
+
+for($count = 1 ; $count <= 10 ; $count++){
+    echo "PHP ";
+    if($count == 6){
+        continue;
+    }
+    echo "is ";
+    echo "awesome<br><br>";
+   /* if($count != 6){
+        echo "is ";
+        echo "awesome<br><br>";
+    }*/
+   
 }
 
 ?>
