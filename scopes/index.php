939f429a1dee109cdbe646e9fbd6fbae5add5bc8
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-20 14:18:03 +0600

    global scopes

--- scopes/index.php
+++ scopes/index.php
@@ -1,21 +1,27 @@
 <!-- =================Local scope=============================== -->
 
 <?php
-function sum() {
+/*function sum() {
     $first_num = 10;
     $second_num = 20;
     $result =  $first_num + $second_num;
     echo $result;
 }
-sum();
+sum();*/
 ?>
 
 <!-- =================Global scope=============================== -->
 
 <?php
-
-
-
+$test_var = "Test";
+function my_name(){
+    global $test_var;
+    global $my_name;
+$my_name = "Puja";
+echo $test_var;
+}
+my_name();
+echo $my_name;
 ?>
 
 <!-- =================Parameter scope=============================== -->
