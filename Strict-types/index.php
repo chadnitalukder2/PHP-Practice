f5271010c20951e54f62a002180bf9a36c31a0cc
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-20 14:08:23 +0600

    scopes

--- Strict-types/index.php
+++ Strict-types/index.php
@@ -1,9 +1,19 @@
 <?php
-declare(static_type-1);
+/*declare(static_type-1);
 function sum(int $a, int $b){
 $c = $a + $b;
 echo "The result is : $c";
 }
-sum(5, 6);
+sum(5, 6);*/
+
+?>
+<!-- ========================================== -->
+
+<?php
+function Full_name( $first, $last ){
+echo  $first. " " .$last;
+}
+
+Full_name( last: "Talukder", first: "Puja");
 
 ?>
